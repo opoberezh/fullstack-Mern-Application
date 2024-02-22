17915606451b1d51e9ebf61ea4615056a1634076
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-22 23:22:14 +0100

    created routes and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePge";
+import ProfilePage from "./pages/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import CssBaseLine from "@mui/material/CssBaseline";
+import {createTheme,  ThemeProvider} from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine/>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/profile/:userId" element={<ProfilePage/>}/>
+      </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
