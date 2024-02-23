94d49574b191b17d6953091df39ce808ecfbbd08
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-23 09:11:32 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="fullstack-Mern-Application">
+        <BrowserRouter basename="fullstack-Mern-Application/client">
           <App />
         </BrowserRouter>
       </PersistGate>
