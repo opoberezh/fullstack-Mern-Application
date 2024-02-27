a1d7a7faa6fdb664fa3023eb8842155a7a21b84e
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-27 19:21:43 +0100

    widgetWrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,36 @@
-import {  Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePge";
-import ProfilePage from "./pages/ProfilePage";
-import CssBaseLine from "@mui/material/CssBaseline";
-import {  ThemeProvider, createTheme } from "@mui/material/styles";
-import { useSelector } from "react-redux";
-import { useMemo } from "react";
-import { themeSettings } from "./theme";
-import { selectTheme } from "./redux/selectors";
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePge';
+import ProfilePage from './pages/ProfilePage';
+import CssBaseLine from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { selectIsToken, selectTheme } from './redux/selectors';
 
 function App() {
   const mode = useSelector(selectTheme);
-  console.log("Current mode:", mode);
+  console.log('Current mode:', mode);
 
-  const theme = useMemo(() =>
-  createTheme(themeSettings(mode)), [mode]);
-const isAuth = 
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector(selectIsToken));
   return (
     <div className="app">
-        <ThemeProvider theme={theme}>
-        <CssBaseLine/>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/profile/:userId" element={<ProfilePage/>}/>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
+        </Routes>
       </ThemeProvider>
-
     </div>
   );
 }
--- client/src/redux/selectors.js
+++ client/src/redux/selectors.js
@@ -3,3 +3,5 @@
 export const selectTheme = state => state.theme.mode;
 
 export const selectUser = state => state.auth.user;
+
+export const selectIsToken = state => state.auth.token;
