dde9756d86ae30c64001483e2e21d1e47fec1d55
Olha Poberezhna <124092276+opoberezh@users.noreply.github.com>
2024-02-28 17:32:34 +0100

    posts widget

--- client/src/pages/HomePge.jsx
+++ client/src/pages/HomePge.jsx
@@ -3,6 +3,8 @@ import Navbar from "components/Navbar";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/selectors";
 import UserWidget from "./widgets/UserWidget";
+import MyPostWidget from "./widgets/MyPostWidget";
+
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -19,6 +21,11 @@ const HomePage = () => {
     >
       <Box flexBasis= {isNonMobileScreens ? "26%" : undefined}>
         <UserWidget userId={_id} picturePath={picturePath}/>
+      </Box>
+      <Box flexBasis={isNonMobileScreens ? "42%" : "2rem"}>
+        <MyPostWidget picturePath={picturePath}/>
+        
+
       </Box>
 <Box flexBasis= {isNonMobileScreens ? "42%" : undefined}
 mt={isNonMobileScreens ? undefined : "2rem"}
